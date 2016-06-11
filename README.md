e8b8a9789e92bcbd3fde1a321d3f745891c0dc8a
michellejanosi <michelle.janosi@gmail.com>
2016-06-11 10:08:00 -0400

    Added solution to coffee and code

---
+++ README.md
@@ -0,0 +1,3 @@
+Coffee and Code
+
+An interpretation of ‘coffee and code’ with emojis.
---
+++ coffee_code.rb
@@ -0,0 +1,13 @@
+# Coffee and code with emojis
+
+:person_frowning: == :sleeping:
+until :person_frowning: == :ok_woman:
+  return :coffee:
+  :person_frowning: += :smile:
+end
+puts :person_frowning:
+
+output =>
+:person_frowning:: :sleeping:
+:person_frowning:: :neutral_face:
+:person_frowning:: :ok_woman:
